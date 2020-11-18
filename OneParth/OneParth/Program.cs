dc159cfacac272c44623cb6cf955f4c0163b5630
Anya394 <pstebl99@mail.ru>
2020-11-18 19:36:02 +0400

    Вычисление полной площади

---
+++ OneParth/OneParth/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OneParth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculate = new Calculate();
+            Console.WriteLine("Введите размеры в формате: \nДлина \nШирина");
+            var totalArea = calculate.TotalArea(Double.Parse(Console.ReadLine()), Double.Parse(Console.ReadLine()));
+            Console.WriteLine("Полная площадь: {0}", totalArea);
+        }
+    }
+    
+    public class Calculate
+    {
+        public double TotalArea(double length, double width)
+        {
+            return length * width;
+        }
+    }
+}
